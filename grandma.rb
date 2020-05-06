3fb1af27d8493e7ced8c74d777827f5284092bfe
Nguyen Ros <nynha0526@gmail.com>
2020-05-06 04:07:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase)
-  if phrase == 'I LOVE YOU GRANDMA!'
-    return 'I LOVE YOU TOO PUMPKIN!'
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
   end
   if phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
