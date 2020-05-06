9240db496303e82a818bbfcd667a11230944a694
Nguyen Ros <nynha0526@gmail.com>
2020-05-06 04:01:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,1 @@
-speak_to_grandma = "I love you grandma".upcase
-
-if speak_to_grandma == "I love you grandma".upcase
-    puts "I love you too pumpkin".upcase
-  
-  elsif speak_to_grandma == "did you eat today?".upcase
-    puts "No, not since 1983!".upcase
-  
-  else
-    puts "Huh?! Speak up, sonny!".upcase
-
-end
-
+def speak_to_grandma(phrase)
